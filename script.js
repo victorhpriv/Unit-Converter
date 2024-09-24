a6e01b937deb7ac123f47f49894e50403dd57881
Victor Hugo Constantino <victorhugosilvac3@gmail.com>
2024-09-23 23:22:37 -0300

    Code convert

---
+++ script.js
@@ -0,0 +1,22 @@
+function convert(){
+  const inputValue = document.getElementById('userInput').value;
+  const unit = document.getElementById("unit").value;
+  const milesToKm = (unit === "milesToKm");
+  
+  
+  let result = 0;
+  
+  if (milesToKm === true) {
+   result = inputValue * 1.60934;
+  } else {
+    result = inputValue / 1.60934;
+  }
+  
+  const resultString = inputValue + "miles are " + result + "km"; 
+  console.log(resultString);
+  
+  const resultElement = document.getElementById('resultElement');
+  resultElement.innerHTML = resultString;
+  };
+  
+  
